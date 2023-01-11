862858cd60f26c5913cee740ae1434728a16dfcc
Ryan Andujar <randujar711@gmail.com>
2023-01-11 18:07:43 -0500

    landing page layout

--- client/src/Map.jsx
+++ client/src/Map.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function Map () {
     return (
-        <div>
-            <img src="" alt="" />
+        <div style={{"width" : "70%", "height" : "70%", "background": "yellow"}}>
+            <img src="/src/assets/khoratan-nemeris-mapou.jpeg" style={{"width" : "100%", "height" : "auto"}}/>
         </div>
     )
 }
+export default Map
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'React'
 
 function NavBar (){
     return(
-        <div style={{"background":"red"}}>
+        <div style={{"background":"yellow", 'height' : "20%", 'display' :'flex'}}>
             <h1>Title</h1>
-            <img src="" alt="" />
+            <img style={{"width" : "auto", "height" : "50%"}} src="src/assets/devcrazzy.randomchat_1.png"/>
         </div>
     )
 }
