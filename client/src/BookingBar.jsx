aceb9f7ccc492d20c2931ab356d1c0e05a206a8e
Ryan Andujar <randujar711@gmail.com>
2023-01-11 17:42:54 -0500

    initial

---
+++ client/src/BookingBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BookingBar () {
+    return(
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookingBar
