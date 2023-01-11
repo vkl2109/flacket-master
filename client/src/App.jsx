0c7fdcef0fdde2e3ab16efb884354f6316cc0aaf
Ryan Andujar <randujar711@gmail.com>
2023-01-11 18:32:25 -0500

    first routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import LandingPage from './LandingPage'
 import './App.css'
+import LoginPage from './LoginPage'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
-    <LandingPage/>
+    <>
+    <BrowserRouter >
+      <Routes>
+        <Route path ={'/'} element ={<LoginPage/>}/>
+        <Route path ={'/landing'} element ={<LandingPage/>}/>
+      </Routes>
+    </BrowserRouter>
+      
+    </>
+    
   )
 }
 
