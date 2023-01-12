a75fba729b9004180d1fb770d291dfe7591c9b80
Vincent <vkl2109@columbia.edu>
2023-01-12 13:42:31 -0500

    simple styling changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
 
   return (
-    <>
-    <BrowserRouter >
-      <Routes>
-        <Route path ={'/'} element ={<LoginPage/>}/>
-        <Route path ={'/landing'} element ={<LandingPage/>}/>
-      </Routes>
-    </BrowserRouter>
-      
-    </>
+    <div className="App">
+      <BrowserRouter >
+        <Routes>
+          <Route path ={'/'} element ={<LoginPage/>}/>
+          <Route path ={'/landing'} element ={<LandingPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
     
   )
 }
