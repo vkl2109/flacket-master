77f933b67e71ee3639dfc15f71389d7088790cce
Chris Li <chris.lty07@gmail.com>
2023-01-12 14:45:15 -0500

    Added Login and Sign Up components

--- client/src/LoginPage.jsx
+++ client/src/LoginPage.jsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./Navbar";
 import { Link } from "react-router-dom";
 
-function LoginPage () {
-    return(
+function LoginPage() {
+    const [loginData, setLoginData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (e) => {
+        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("logging in...")
+
+        // const login =  async() => {
+        //     let req = await fetch("/login")
+        //     let res = await req.json()
+        //     console.log(res)
+        // }
+    }
+
+    return (
         <div>
-            <NavBar />
-            <Link to = {"/landing"}>
-                <div>
-                    Landing Page Link 
-                </div>
+            <Link to={"/"}>
+                Landing Page Link
             </Link>
-            
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="username"
+                        name="username"
+                        value={loginData.username}
+                        onChange={handleChange}>
+                    </input>
+
+                    <input
+                        type="password"
+                        placeholder="password"
+                        name="password"
+                        value={loginData.password}
+                        onChange={handleChange} >
+                    </input>
+                    <input
+                        type="submit"
+                        value="login"
+                    >
+                    </input>
+                </form>
+
+            </div>
         </div>
     )
 }
