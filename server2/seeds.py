5e6091f43d3452fb081b98bd77521311b118713e
Vincent <vkl2109@columbia.edu>
2023-01-13 10:28:03 -0500

    updated seeds file to drop records before re creating

--- server2/seeds.py
+++ server2/seeds.py
@@ -1,24 +1,27 @@
 from app import app
 from models import db, User, Booking, Classroom, Event, Seat
 
-print('Seeding database ... 🌱')
-# Add your seed data here
-user1 = User('test123', 'test@example.com','12345')
-db.session.add(user1)
-db.session.commit()
-
-rooms = ['Turing', 'Collins', 'Kay']
-event_names = ['Rails', 'Python', 'React']
-
-for room in rooms:
-    new_classroom = Classroom(room)
-    for e in event_names:
-        new_event = Event(e, '2023-01-13 12:00:00.000', '2023-01-14 13:00:00.000', new_classroom.id)
-        for i in range(30):
-            new_seat = Seat()
-        db.session.add(new_event)
-        db.session.commit()
-    db.session.add(new_classroom)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    print('Seeding database ... 🌱')
+    # Add your seed data here
+    user1 = User('test123', 'test@example.com','12345')
+    db.session.add(user1)
     db.session.commit()
 
-print('Done! 🌳')
+    rooms = ['Turing', 'Collins', 'Kay']
+    event_names = ['Rails', 'Python', 'React']
+
+    # for room in rooms:
+    #     new_classroom = Classroom(room)
+    #     for e in event_names:
+    #         new_event = Event(e, '2023-01-13 12:00:00.000', '2023-01-13 13:00:00.000', new_classroom.id)
+    #         for i in range(30):
+    #             new_seat = Seat(new_event.id, i, False)
+    #         db.session.add(new_event)
+    #         db.session.commit()
+    #     db.session.add(new_classroom)
+    #     db.session.commit()
+
+    print('Done! 🌳')
