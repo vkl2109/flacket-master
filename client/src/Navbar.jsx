87fd489110643d2b5dcfed391950cac8ed449d88
Ryan Andujar <randujar711@gmail.com>
2023-01-13 10:04:00 -0500

    test push

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function NavBar (){
+function NavBar(){
         
     return(
-        <div style={{"background":"yellow", 'height' : "100%", 'display' :'flex', 'justifyContent': 'space-between', 'alignItems': 'center'}}>
+        <div className='nav-cont' style={{"background":"yellow", 'height' : "100%", 'display' :'flex', 'justifyContent': 'space-between', 'alignItems': 'center'}}>
             <h1>Title</h1>
             <Link to ={"/"}> 
                 <img style={{"width" : "auto", "height" : "100%"}} src="src/assets/devcrazzy.randomchat_1.png"/>
