3ca9d4a6afb15ff07183d853c5681b8c0b309a78
Chris Li <chris.lty07@gmail.com>
2023-01-17 11:26:42 -0500

    Sign Up Login Form Toggle

--- client/src/LoginPage.jsx
+++ client/src/LoginPage.jsx
@@ -1,26 +1,56 @@
 import React, { useState } from "react";
-import NavBar from "./Navbar";
-import { Link } from "react-router-dom";
 
 function LoginPage() {
+    const [showSignUp, setShowSignUp] = useState(false);
     const [loginData, setLoginData] = useState({
         username: "",
         password: ""
     });
+    const [signUpData, setSignUpData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        avatarURL: ""
+    })
 
-    const handleChange = (e) => {
+    const handleLoginInput = (e) => {
         setLoginData({ ...loginData, [e.target.name]: e.target.value });
+    }
+
+    const handleSignUpInput = (e) => {
+        setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("logging in...")
 
-        // const login =  async() => {
-        //     let req = await fetch("/login")
-        //     let res = await req.json()
-        //     console.log(res)
-        // }
+        if (showSignUp) {
+            console.log("Signing up...")
+            // const signup = async() => {
+            //     let req = await fetch("/login")
+            //     let res = await req.json()
+            //     console.log(res)
+            //     if (res.ok) {
+            //          navigate to home page
+            //          set user to logged in user    
+            //     }
+            // }
+
+        } else {
+            console.log("Loggin in...")
+            // const login =  async() => {
+            //     let req = await fetch("/login")
+            //     let res = await req.json()
+            //     console.log(res)
+            //      if (res.ok) {
+            //          set user to logged in user
+            //          navigate to home page
+            //      }
+            // }
+
+        }
+
     }
 
     return (
@@ -29,55 +59,120 @@ function LoginPage() {
                 <img src="src/assets/00bootcamp2-jumbo.jpeg" />
             </div>
             <div className="form-cont">
-                <form onSubmit={handleSubmit}>
-                <div input-group='true' mb-3='true'>
-                    <input
-                        class="form-control"
-                        type="text"
-                        placeholder="Username"
-                        name="username"
-                        value={loginData.username}
-                        onChange={handleChange}>
-                    </input>
-                </div>
-                <div>
-                    <input
-                        class="form-control"
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={loginData.password}
-                        onChange={handleChange} >
-                    </input>
-                </div>
-
-                <div>
-                    <input
-                        class="form-control"
-                        type="submit"
-                        value="login"
-                    >
-                    </input>
-                </div>
-                <h6 style={{'alignSelf':'center'}}>or</h6>
-                <div>
-                    <input
-                        class="form-control"
-                        type="submit"
-                        value="Signup"
-                    >
-                    </input>
-                </div>
-                <Link to = {"/home"}>
-                    <div>
-                        Home Page Link 
+                {!showSignUp && <form onSubmit={handleSubmit}>
+                    <div input-group='true' mb-3='true'>
+                        <input
+                            className="form-control"
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                            value={loginData.username}
+                            onChange={handleLoginInput}>
+                        </input>
                     </div>
-                </Link>
-                </form>
-                
-            </div>
+                    <div>
+                        <input
+                            className="form-control"
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={loginData.password}
+                            onChange={handleLoginInput} >
+                        </input>
+                    </div>
+                    <div>
+                        <input
+                            className="form-control"
+                            type="submit"
+                            value="login"
+                        >
+                        </input>
+                    </div>
+                    <h6 style={{ 'alignSelf': 'center' }}>or</h6>
+                    <div>
+                        <button
+                            onClick={() => setShowSignUp(true)}
+                            className="form-control"
+                        >
+                            Sign Up
+                        </button>
+                    </div>
 
-            
+
+                </form>}
+
+                {showSignUp && <>
+                    <form onSubmit={handleSubmit}>
+                        <div input-group='true'>
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="username"
+                                name="username"
+                                value={signUpData.username}
+                                onChange={handleSignUpInput}
+                            >
+                            </input>
+                        </div>
+                        <div>
+                            <input
+                                className="form-control"
+                                type="email"
+                                placeholder="email"
+                                name="email"
+                                value={signUpData.email}
+                                onChange={handleSignUpInput}
+                            >
+                            </input>
+                        </div>
+                        <div>
+                            <input
+                                className="form-control"
+                                type="password"
+                                placeholder="password"
+                                name="password"
+                                value={signUpData.password}
+                                onChange={handleSignUpInput}
+                            >
+                            </input>
+                        </div>
+                        <div>
+                            <input
+                                className="form-control"
+                                type="password"
+                                placeholder="confirm password"
+                                name="passwordConfirm"
+                                value={signUpData.passwordConfirm}
+                                onChange={handleSignUpInput}
+                            >
+                            </input>
+                        </div>
+                        <div>
+                            <button
+                                onClick={() => setShowSignUp(false)}
+                                className="form-control"
+                            // type="submit"
+                            // value="login"
+                            >
+                                Login
+                            </button>
+                        </div>
+                        <h6 style={{ 'alignSelf': 'center' }}>or</h6>
+                        <div>
+                            <input
+                                onClick={() => setShowSignUp(true)}
+                                className="form-control"
+                                type="submit"
+                                value="Signup"
+                            >
+                            </input>
+                        </div>
+                    </form>
+
+                </>}
+
+
+            </div>
 
         </div>
     )
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function NavBar(){
-        
-    return(
+function NavBar() {
+
+    return (
         <div className='nav-cont'>
-            <h1>FlatPass</h1>
-            <Link to ={"/"}> 
-                <img src="src/assets/avatardefault_92824.png"/>
+            <Link to={"/home"} style={{ textDecoration: "none" }}>
+                <h1>FlatPass</h1>
+            </Link>
+            <Link to={"/"}>
+                <img src="src/assets/avatardefault_92824.png" />
             </Link>
         </div>
     )
