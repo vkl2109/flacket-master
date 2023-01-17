4c170d3aa938191d99ed6c9f489b9d0fe1220a0f
Chris Li <chris.lty07@gmail.com>
2023-01-17 14:14:04 -0500

    Add map click function

--- client/src/EventList.jsx
+++ client/src/EventList.jsx
@@ -1,90 +1,90 @@
 import { useState, useEffect } from 'react'
 import './css/eventlist.css'
 
-const EventList = () => {
+const EventList = ({ selectedRoom }) => {
 
-    const [ classrooms, setClassrooms ] = useState()
-    const [ isLoading, setIsLoading ] = useState(true)
-    const [ searchInput, setSearchInput ] = useState("")
-    const [ reservation, setReservations ] = useState([])
-    const [ modalOpen, setModalOpen ] = useState(false)
+    const [classrooms, setClassrooms] = useState()
+    const [isLoading, setIsLoading] = useState(true)
+    const [searchInput, setSearchInput] = useState("")
+    const [reservation, setReservations] = useState([])
+    const [modalOpen, setModalOpen] = useState(false)
 
     const seedData = [
         {
-            "name" : "Turing",
-            "events" : [
+            "name": "Turing",
+            "events": [
                 {
-                    "name" : "React",
+                    "name": "React",
                     "start_time": "10AM",
                     "seats": "30"
                 },
                 {
-                    "name" : "Rails",
+                    "name": "Rails",
                     "start_time": "1PM",
                     "seats": "30"
                 },
                 {
-                    "name" : "DS&A",
+                    "name": "DS&A",
                     "start_time": "5PM",
                     "seats": "30"
                 }
             ]
-            
+
         },
         {
-            "name" : "Kay",
-            "events" : [
+            "name": "Kay",
+            "events": [
                 {
-                    "name" : "React",
+                    "name": "React",
                     "start_time": "10AM",
                     "seats": "30"
                 },
                 {
-                    "name" : "Rails",
+                    "name": "Rails",
                     "start_time": "1PM",
                     "seats": "30"
                 },
                 {
-                    "name" : "DS&A",
+                    "name": "DS&A",
                     "start_time": "5PM",
                     "seats": "30"
                 }
             ]
-            
+
         },
         {
-            "name" : "Collins",
-            "events" : [
+            "name": "Collins",
+            "events": [
                 {
-                    "name" : "React",
+                    "name": "React",
                     "start_time": "10AM",
                     "seats": "30"
                 },
                 {
-                    "name" : "Rails",
+                    "name": "Rails",
                     "start_time": "1PM",
                     "seats": "30"
                 },
                 {
-                    "name" : "DS&A",
+                    "name": "DS&A",
                     "start_time": "5PM",
                     "seats": "30"
                 }
             ]
-            
+
         }
     ]
 
     const handleChange = (e) => setSearchInput(e.target.value);
 
     const reserve = (reserve) => {
-        if (reservation.includes(reserve)) return 
+        if (reservation.includes(reserve)) return
         setReservations([...reservation, reserve])
         console.log(reservation)
         setModalOpen(true)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         // const request = async () => {
         //     let req = await fetch("http://localhost:5000/classrooms")
         //     let res = await req.json()
@@ -96,69 +96,87 @@ const EventList = () => {
         // request()
         setClassrooms(seedData)
         setIsLoading(false)
-    },[])
+    }, [])
     console.log(modalOpen)
 
     const modal = document.getElementById("exampleModal")
+    console.log('hi')
 
-    return(
+    useEffect(() => {
+        if (!selectedRoom) {
+            console.log('hi')
+            return
+        } else {
+            console.log('hi')
+            setClassrooms(classrooms.filter(room => room.room.includes("Turing")))
+        }
+    }, [selectedRoom])
+
+    return (
         <div className="eventList">
             {/* <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
             </button> */}
 
-{/* <!-- Modal --> */}
-        {modalOpen ? <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-            <div className="modal-content">
-            <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body">
-                TEST BODY
-            </div>
-            <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
-            </div>
-            </div>
-        </div>
-        </div> : null
-        }
+            {/* <!-- Modal --> */}
+            {modalOpen ? <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            TEST BODY
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div> : null
+            }
             <input
                 className='searchbar'
                 type="search"
                 placeholder="Search here"
                 onChange={handleChange}
                 value={searchInput} />
-            {isLoading ? "Loading..." : classrooms.filter(room => room.name.includes(searchInput)).map(classroom => {
-                return (
-                    <div className="table">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>{classroom.name}:</th>
-                                    <th></th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {classroom.events.map(event => (
-                                    <tr key={event.name+event.start_time}
-                                        data-bs-toggle="modal" 
-                                        data-bs-target="#exampleModal"
-                                    >
-                                        <td>{event.name}</td>
-                                        <td onClick={() => {reserve(event)}}>{event.start_time}</td>
-                                        <td>{event.seats}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )
-            })}
+            {isLoading ? "Loading..." :
+                classrooms
+                    .filter(room => {
+                        // console.log("in first filter")
+                        return room.room.includes(selectedRoom)
+                    })
+                    .filter(room => room.name.includes(searchInput))
+                    .map(classroom => {
+                        return (
+                            <div className="table" >
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>{classroom.name}:</th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {classroom.events.map(event => (
+                                            <tr key={event.name + event.start_time}
+                                                data-bs-toggle="modal"
+                                                data-bs-target="#exampleModal"
+                                            >
+                                                <td>{event.name}</td>
+                                                <td onClick={() => { reserve(event) }}>{event.start_time}</td>
+                                                <td>{event.seats}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        )
+                    })}
         </div>
     )
 }
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,16 +5,22 @@ import BookingBar from "./BookingBar";
 import Map from "./Map";
 import EventList from "./EventList_mod";
 
-function Home(){
-    const [ reservation, setReservations ] = useState([])
+function Home() {
+    const [reservation, setReservations] = useState([]);
+    const [selectedRoom, setSelectedRoom] = useState("");
 
     return (
         <div className='main-cont'>
             <div className='sub-cont'>
-                <EventList reservation={reservation} setReservations={setReservations}/>
-                <div style={{"display":"flex", "flexDirection":"column", "alignItems":'center'}}>
-                    <Map />
-                    <BookingBar booking={reservation}/>
+                <EventList
+                    reservation={reservation}
+                    setReservations={setReservations}
+                    selectedRoom={selectedRoom}
+                    setSelectedRoom={setSelectedRoom}
+                />
+                <div style={{ "display": "flex", "flexDirection": "column", "alignItems": 'center' }}>
+                    <Map selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} />
+                    <BookingBar booking={reservation} />
                 </div>
             </div>
         </div>
