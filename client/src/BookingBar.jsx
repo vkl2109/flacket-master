6f0763f5250b16f6d5b43a7cfe75ebba7d03885b
Ryan Andujar <randujar711@gmail.com>
2023-01-18 13:49:54 -0500

    carasoul repushed

--- client/src/BookingBar.jsx
+++ client/src/BookingBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import BookingCard from './BookingCard'
 import './css/booking.css'
 
