6f3ef1ca7605398129a7d874cedc169f3457af1d
Vincent <vkl2109@columbia.edu>
2023-01-18 10:25:12 -0500

    adjusted css for booking bar

--- client/src/BookingCard.jsx
+++ client/src/BookingCard.jsx
@@ -3,11 +3,12 @@ import './css/booking.css'
 const BookingCard = ({booking}) => {
     return (
         <div className="bookingCard">
-            <h4>
+            <h6>
                 {booking.event}
-            </h4>
+            </h6>
             <p>{booking.classroom}</p>
-            <p>{booking.start_time}</p>
+            <p>{booking.start_time.split(',')[0]}</p>
+            <p>{booking.start_time.split(',')[1].slice(0, -3)}</p>
         </div>
     )
 }
